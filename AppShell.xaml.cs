feede13c8cdf3d83d738fde8be1366a8cb145e37
pradip.shrestha <pradip.shrestha@np.technofex.com>
2024-05-21 07:34:03 +0545

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Notes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+        }
+    }
+}
